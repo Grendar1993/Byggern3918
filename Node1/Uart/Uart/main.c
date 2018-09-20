4c987931100523208de9ee68cef852ba8a50b3a3
Grendar1993 <35447018+Grendar1993@users.noreply.github.com>
2018-09-20 18:12:47 +0200

    Added joystick driver

--- Node1/Uart/Uart/main.c
+++ Node1/Uart/Uart/main.c
@@ -1,11 +1,3 @@
-/*
- * Uart.c
- *
- * Created: 04.09.2018 09.43.47
- * Author : Grendar
- */ 
-
-
 #define F_CPU 4912500 /*4912500UL*/
 #define BAUD 9600
 #define UBRREG F_CPU/16/BAUD-1
@@ -15,6 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "UART.h"
+#include "Joystick.h"
 
 void SRAM_test(void);
 
