28d05f3fac146a86db445012291a11eda2ceea87
MaMon89 <43442763+MaMon89@users.noreply.github.com>
2018-11-19 12:13:51 +0100

    github teit

--- NODE2atmega/NODE2atmega/adc.c
+++ NODE2atmega/NODE2atmega/adc.c
@@ -7,27 +7,32 @@
 
 #include "adc.h"
 
+/*
+This driver enables the use of the ADC for node 2
+*/
+
+
+//This function sets up the ADC for use
 void ADC_init( void )
 {
 	// Enable ADC and set prescaler to 128
 	ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 	ADCSRA &= ~(1<<ADIF);
 	ADCSRA &= ~(1<<ADIE);
-	//ADCSRB &= ~(1 << MUX0); 
-	// Reference selection: AVCC w/ external capacitor at AREF. Left adjust result
+	
+	// Reference selection, internal Vref
 	ADMUX |= (1 << REFS0) | (1 << ADLAR);
 	
 	// Start conversion
 	ADCSRA |= (1 << ADSC);
 	
-	printf("adc value is %d \n\r", ADCH);
 }
 
+
+//function that starts the ADC conversion
 uint8_t ADC_read( void )
 {
-	
-	
-	
+		
 	// Start conversion
 	ADCSRA |= (1 << ADSC);
 	
@@ -35,6 +40,5 @@ uint8_t ADC_read( void )
 	while(ADCSRA & (1 << ADSC));
 	
 	// Read converted data
-	printf("adc value is %d \n\r", ADCH);
 	return ADCH;
 }
